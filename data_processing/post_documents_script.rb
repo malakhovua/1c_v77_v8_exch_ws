979716f3b79734cb14f5ba7f264b41a966593963
malakhovua <malakhovua@gmail.com>
2023-07-05 21:15:53 +0300

    05072023

---
+++ data_processing/post_documents_script.rb
@@ -0,0 +1,5 @@
+require 'C:\www\root\sinatra_1c_77\data_processing\post_documents.rb'
+
+post = Post_documents.new
+post.execute
+
---
+++ data_processing/recalc_cost_packs_script.rb
@@ -0,0 +1,4 @@
+require 'C:\www\root\sinatra_1c_77\data_processing\recalc_cost_packs.rb'
+
+recalc = Recalc_cost_packs.new
+recalc.execute
