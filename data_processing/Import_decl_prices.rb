10d61950d61e24deab5f0584f24cd870b58e129a
malakhovua <malakhovua@gmail.com>
2024-10-14 13:27:04 +0300

    DateTime.Now.ToString(yyyy/MM/dd HH:mm:ss)

---
+++ data_processing/Import_decl_prices.rb
@@ -0,0 +1,8 @@
+#СинхронизироватьДекларируемыеЦены();
+#
+require 'C:\www\root\sinatra_1c_77\com\1c_obj.rb'
+conn = Application_v7.new
+conn.create
+conn.connect
+@v7 = conn.v7
+@V7.СинхронизироватьДекларируемыеЦены()
