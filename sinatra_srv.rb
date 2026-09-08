1f0e451d93d577b4ab59b0a8e4704e0382ab89c5
malakhovua <malakhovua@gmail.com>
2026-09-08 16:40:51 +0300

    DateTime.Now.ToString(yyyy/MM/dd HH:mm:ss)

--- sinatra_srv.rb
+++ sinatra_srv.rb
@@ -184,16 +184,18 @@ end
 
 # ===================END GET REPORTexitS=========================================================
 
-get '/work/documents/:name/:number/:date/:group_code' do
+get '/work/documents/:name/:number/:date/:group_code/:date2' do
 
   get_connect
 
   date_doc = @V7.StringToDate(params[:date][0...10])
 
-
-  if params[:name] =='client_order'
+  if params[:name] == 'client_order'
     if params[:group_code] != "empty"
       result = @V7.GetOrderByClientGroupCode(params[:group_code], date_doc)
+    elsif params[:date2] != "empty"
+      date_2 = @V7.StringToDate(params[:date2][0...10])
+      result = @V7.GetOrderByPeriod(date_doc,date_2)
     else
       result = @V7.GetOrderByNumber(params[:number], date_doc)
     end
